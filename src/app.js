646e6e437f0382843a01d7192b528a2fbd648ac8
Boris Dinkevich <boris@dinkevich.com>
2015-11-14 22:33:37 +0200

    Newer angular2 versions allows 'templates' to be inside the Component decorator

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import 'zone.js';
 import 'reflect-metadata';
 import { bootstrap, Component, View } from 'angular2/angular2';
 
-@Component({ selector: 'my-app' })
-@View({
+@Component({ 
+  selector: 'my-app',
   template: `
 <h1>Welcome to your Angular2 + Webpack + ES6 Playground</h1>
 <h2>Have {{ what }}</h2>
