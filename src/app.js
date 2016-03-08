2960fc26db08efc20c5e9c8670ab76aa98049c4b
Boris Dinkevich <boris@dinkevich.com>
2016-03-08 10:08:35 +0200

    Remove un-needed imports

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Component, View } from 'angular2/core';
+import { Component } from 'angular2/core';
 
 @Component({
   selector: 'my-app',
@@ -11,4 +11,4 @@ export default class App {
   constructor() {
     this.what = "a good time!";
   }
-}
+};
