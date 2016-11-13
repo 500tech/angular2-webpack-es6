95db0de098e858b7ecbddab280e718ab4b6f2fdf
Boris Dinkevich <boris@dinkevich.com>
2016-11-13 17:41:14 +0200

    Update to latest Angular version (2.1.0)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,21 @@
 import 'reflect-metadata';
-import {bootstrap} from 'angular2/platform/browser';
+import 'zone.js';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { enableProdMode } from '@angular/core';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import App from './app';
 
-bootstrap(App);
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [
+    App
+  ],
+  bootstrap: [ App ]
+})
+class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
